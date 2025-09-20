3599f49faec98acffb897850b807d8f6d996cc89
olalesam <olalesam87@gmail.com>
2025-09-20 17:57:32 +0000

    first commit

---
+++ src/main/java/com/olale/user_microservice/UserMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.olale.user_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceApplication.class, args);
+	}
+
+	// comment
+
+}
